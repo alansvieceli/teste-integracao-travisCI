694c8dce98c1b65c936ba0079099232b8fb8d68b
Alan Vieceli <alansvieceli@gmail.com>
2018-10-08 12:37:30 -0300

    definindo e usando arquivo application.properties

--- src/main/java/com/alanvieceli/api/TesteIntegradoComTravisCiApplication.java
+++ src/main/java/com/alanvieceli/api/TesteIntegradoComTravisCiApplication.java
@@ -1,12 +1,25 @@
 package com.alanvieceli.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TesteIntegradoComTravisCiApplication {
+	
+	@Value("${paginacao.qtd_por_pagina}")
+	private int qtdePorPagina;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TesteIntegradoComTravisCiApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args ->{
+			System.out.println("### Quantidade de elementos por página = " + this.qtdePorPagina);
+		};
 	}
 }
